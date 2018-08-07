d8a9c8e477700a5059536304a08c6bc73983707a
Chris Chabilall <chris.c@vanillaforums.com>
2018-08-07 15:38:04 -0400

    fixed indenting

--- sso.php
+++ sso.php
@@ -8,19 +8,19 @@
  */
 $VFRequest = vf_get_value('VFRequest', $_GET);
 switch ($VFRequest) {
-	// Show the signed in user
-	case 'connect':
-      require_once dirname(__FILE__).'/functions.jsconnect.php';
-      $user = vf_get_user();
-      $options = get_option(VF_OPTIONS_NAME);
-      $clientID = vf_get_value('sso-clientid', $options, '');
-      $secret = vf_get_value('sso-secret', $options, '');
-      WriteJsConnect($user, $_GET, $clientID, $secret, true);
-      exit();
-      break;
-	// Generate a secret to be used for security.
-	case 'generate-secret':
-		echo wp_generate_password(64, true, true);
-		exit();
-		break;
+    // Show the signed in user
+    case 'connect':
+        require_once dirname(__FILE__).'/functions.jsconnect.php';
+        $user = vf_get_user();
+        $options = get_option(VF_OPTIONS_NAME);
+        $clientID = vf_get_value('sso-clientid', $options, '');
+        $secret = vf_get_value('sso-secret', $options, '');
+        WriteJsConnect($user, $_GET, $clientID, $secret, true);
+        exit();
+        break;
+    // Generate a secret to be used for security.
+    case 'generate-secret':
+        echo wp_generate_password(64, true, true);
+        exit();
+        break;
 }
